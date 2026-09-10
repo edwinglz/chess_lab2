a4f3626b2dd41dd43c83ab6ea368ff54a0b64ac9
edwinglz <edwinglz2006@gmail.com>
2026-09-10 12:09:20 -0600

    Adding user interactinos

--- lab2.java
+++ lab2.java
@@ -2,7 +2,23 @@ import java.util.Scanner;
 
 public class lab2{
     public static void main(String[] args){
-
         
+        Scanner scanner = new Scanner(System.in);
+    
+        //MAIN MENU, SELECTING THE PIECE AND THE POSITIONS
+        System.out.println("Welcome! If you are here this means you are trying to verify a chess piece's movement.");
+        System.out.println("\nStart by typing the chess piece of your choice (in all capitals): " +
+                                "1. PAWN" +
+                                "2. ROOK" +
+                                "3. KNIGHT" +
+                                "4. BISHOP" +
+                                "5. QUEEN" +
+                                "6. KING ");
+        String chosenPiece = scanner.nextLine();
+
+        System.out.println("\nNext, please type the colour of your piece: (In all capitals)" +
+                                "1. WHITE" +
+                                "2. BLACK");
+        String chosenColor = scanner.nextLine();
     }
 }
