a1cb31494a719493c919177665aabc22edb0a1cd
edwinglz <edwinglz2006@gmail.com>
2026-09-10 12:34:25 -0600

    METHOD

--- lab2.java
+++ lab2.java
@@ -1,6 +1,42 @@
 import java.util.Scanner;
 
 public class lab2{
+
+    //gets a char and returns an int limited to A-H to 1-8
+    public static int turnCharToInt(char x){
+        int xcoord;
+
+        if(x=='a'||x=='A'){
+            xcoord = 1;
+        }
+        else if(x=='b'||x=='B'){
+            xcoord = 2;
+        }
+        else if(x=='c'||x=='C'){
+            xcoord = 3;
+        }
+        else if(x=='d'||x=='D'){
+            xcoord = 4;
+        }
+        else if(x=='e'||x=='E'){
+            xcoord = 5;
+        }
+        else if(x=='f'||x=='F'){
+            xcoord = 6;
+        }
+        else if(x=='g'||x=='G'){
+            xcoord = 7;
+        }
+        else if(x=='h'||x=='H'){
+            xcoord = 8;
+        }
+        else{
+            System.err.println("Invalid x-coordinate. Exiting program.");
+            xcoord = -1;
+            System.exit(0);
+        }
+        return xcoord;
+    }
     public static void main(String[] args){
         
         Scanner scanner = new Scanner(System.in);
