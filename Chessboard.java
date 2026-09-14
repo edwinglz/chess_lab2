c1e4c226f4a6b42cc1dd9cc04745ee5a7182c3ab
CarlLion1 <hazososmar@gmail.com>
2026-09-13 18:47:21 -0600

    Update Chessboard.java

--- Chessboard.java
+++ Chessboard.java
@@ -1,3 +1,15 @@
-public class Chessboard{
-    
+public class Chessboard {
+    public static final int MIN_ROW = 1;
+    public static final int MAX_ROW = 8;
+    public static final char MIN_COL = 'a';
+    public static final char MAX_COL = 'h';
+
+    public Chessboard() {
+    }
+
+    public boolean withinChessboard(char column, int row) {
+        char colLower = Character.toLowerCase(column);
+        return (colLower >= MIN_COL && colLower <= MAX_COL) && 
+               (row >= MIN_ROW && row <= MAX_ROW);
+    }
 }
